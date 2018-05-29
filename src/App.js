1adb4b2157c83e4843ca37f50d3e0ad95da91977
Helene Blanchard <erba0614@gmail.com>
2018-05-29 18:37:34 +0200

    Welcome page's aside components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import AsideLeft from './components/AsideLeft.js'
+import AsideRight from './components/AsideRight.js'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
+        <div className="App">
+      <AsideLeft />
+
+
+      <AsideRight />
+
+
+        </div>
+          )
+    }
+
   }
-}
 
 export default App
---
+++ src/components/AsideLeft.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './css/AsideLeft.css'
+
+class AsideLeft extends Component {
+
+  render(){
+         return(
+          <div className="AsideLeft_Line">
+          <span>CATEGORIE</span>
+          <h4>Titre article</h4>
+          <p>blablabla</p>
+           <a href="#lien vers la page de l'article">>>> Lire la suite</a>
+           </div>
+                )
+          }
+}
+
+
+  export default AsideLeft;
