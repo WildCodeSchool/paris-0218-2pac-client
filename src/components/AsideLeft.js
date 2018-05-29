03f95d79ce500dcbfe27384dd9931af864b7ba1f
NadiaWho <36800631+NadiaWho@users.noreply.github.com>
2018-05-29 22:54:36 +0200

    Update AsideLeft.js

--- src/components/AsideLeft.js
+++ src/components/AsideLeft.js
@@ -3,15 +3,15 @@ import './css/AsideLeft.css'
 
 class AsideLeft extends Component {
   render () {
-         return (
-    <div className="AsideLeft_Line">
-      <span>CATEGORIE</span>
+    return (
+      <div className="AsideLeft_Line">
+        <span>CATEGORIE</span>
         <h4>Titre article</h4>
         <p>blablabla</p>
         <a href="#lien vers la page de l'article">>>> Lire la suite</a>
-        </div>
+      </div>
       )
-    }
   }
+}
 
 export default AsideLeft
