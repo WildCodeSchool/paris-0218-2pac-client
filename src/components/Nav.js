190ae90f1fad49255a771c53f55d9af6b8ab0fdc
Edouard de Laage <edouarddelaage@gmail.com>
2018-06-01 16:48:34 +0200

    Display in console sub menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,22 +3,37 @@ import Button from './Button'
 import './Nav.css'
 
 class Nav extends React.Component {
-  state= {
-    item: ['Qui sommes nous ?', 'Comprendre la PAC', 'Notre vision', 'Agir', 'Presse'],
-    menuHowWeAre: false,
-    menuUnderstandPac: false
+  // state= {
+  //   item: ['Qui sommes nous ?', 'Comprendre la PAC', 'Notre vision', 'Agir', 'Presse'],
+  //   menuHowWeAre: false,
+  //   menuUnderstandPac: false
+  // }
+
+  state = {
+    navItems:
+    [
+      {titre: 'Qui sommes nous ?', children: ['Présentation', 'Historique', 'Nos actions', 'Nos membres']},
+      {titre: 'Comprendre la PAC', children: ['L’ABC de la PAC', 'La PAC, ça m’impacte ?', 'La PAC 2015-2020', 'La réforme de la PAC', 'Chiffres-clés', 'Sigles']},
+      {titre: 'Notre vision', children: []},
+      {titre: 'Agir', children: []},
+      {titre: 'Presse', children: []}
+    ]
   }
 
-  toggleList = (event) => {
-    this.setState({menuHowWeAre: true})
-    console.log(this.state.menuHowWeAre)
+  toggleList = (event, i) => {
+    if (this.state.navItems[i].children) {
+      this.state.navItems[i].children.map(e => console.log(e))
+    }
   }
 
   render () {
     return (
       <div className="Nav">
         {
-          this.state.item.map((Item, i) => <Button onClick={(event) => this.toggleList()} key={i} className="Button Item">{Item}</Button>)
+          this.state.navItems.map((Item, i) =>
+            <Button onClick={(event) => this.toggleList(event, i)}
+              key={i} className="Button Item">{Item.titre}
+            </Button>)
         }
       </div>
     )
