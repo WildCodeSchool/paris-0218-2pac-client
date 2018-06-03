c0e8007777075e7264e17113a5eb9da3f3305432
HediK <hedi0475@gmail.com>
2018-06-03 03:39:22 +0200

    footer component complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
 import logo from './img/logo_horizon.svg'
-import './css/footer.css'
+import './footer.css'
 import FaChevronRight from 'react-icons/lib/fa/chevron-right'
+import FaCircle from 'react-icons/lib/fa/circle'
+import FooterMembersLogos from './FooterMembersLogos'
 
 export default class Footer extends Component {
   render () {
     return (
       <footer>
 
-        <div className="footer-desc">
-
-            <div className="logo"><img alt="logo" src={logo} /> </div>
-            <div className="desc">
-              <span> <FaChevronRight className='icon' /> POUR UNE AUTRE PAC </span> {"\n"}
-              est une plateforme française inter-organisations{"\n"}
-              constituant un espace commun de réflexion et d'action,{"\n"}
+        <div className='footer-desc'>
+          <div className='logo'><img alt='logo' src={logo} /> </div>
+          <div className='desc'>
+            <span> <FaChevronRight className='icon' /> POUR UNE AUTRE PAC </span> {'\n'}
+              est une plateforme française inter-organisations{'\n'}
+              constituant un espace commun de réflexion et d'action,{'\n'}
               en vue de la refonte de la politique agricole commune.
-            </div>
-
+          </div>
         </div>
+
+        <div className='members-container'>
+          <div className='intro'> MEMBRES </div>
+          <FooterMembersLogos />
+        </div>
+
+        <div className='mentions'> <a href='/'> <FaCircle /> Mentions légales <FaCircle /> </a> </div>
 
       </footer>
     )
---
+++ src/components/FooterMembersLogos.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import './footer.css'
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
+import actionAid from './img/members_logo/action_aid.png'
+import agter from './img/members_logo/agter.jpg'
+import ape from './img/members_logo/ape.jpg'
+import attac from './img/members_logo/attac.png'
+import bioCons from './img/members_logo/bio_consomacteurs.png'
+import cen from './img/members_logo/cen.jpg'
+import cfsi from './img/members_logo/cfsi.png'
+import civam from './img/members_logo/civam.png'
+import ciwf from './img/members_logo/ciwf.jpg'
+import cmr from './img/members_logo/cmr.jpg'
+import commerceEq from './img/members_logo/commerce_equitable_france.jpg'
+import conf from './img/members_logo/conf.png'
+import fadear from './img/members_logo/fadear.png'
+import fnab from './img/members_logo/fnab.png'
+import fne from './img/members_logo/FNE.jpg'
+import fnh from './img/members_logo/FNH.jpg'
+import fnpnr from './img/members_logo/fnpnr.png'
+import genCob from './img/members_logo/generations_cobayes.jpg'
+import gf from './img/members_logo/gf.png'
+import greenpeace from './img/members_logo/greenpeace.png'
+import isf from './img/members_logo/isf.jpg'
+import lpo from './img/members_logo/lpo.png'
+import miramap from './img/members_logo/miramap.png'
+import mrjc from './img/members_logo/mrjc.png'
+import rac from './img/members_logo/rac.jpg'
+import slowFood from './img/members_logo/slow_food.jpg'
+import sol from './img/members_logo/sol.png'
+import terreDeLiens from './img/members_logo/terre_de_liens.png'
+import terreEtHumanisme from './img/members_logo/terre_et_humanisme.png'
+import unaf from './img/members_logo/unaf.jpg'
+import wwf from './img/members_logo/wwf.jpeg'
+
+export default class FooterMembersLogos extends Component {
+  state = {
+
+    logos: [
+      {src: actionAid, alt: 'member logo'},
+      {src: agter, alt: 'member logo'},
+      {src: ape, alt: 'member logo'},
+      {src: attac, alt: 'member logo'},
+      {src: bioCons, alt: 'member logo'},
+      {src: cen, alt: 'member logo'},
+      {src: cfsi, alt: 'member logo'},
+      {src: civam, alt: 'member logo'},
+      {src: ciwf, alt: 'member logo'},
+      {src: cmr, alt: 'member logo'},
+      {src: commerceEq, alt: 'member logo'},
+      {src: conf, alt: 'member logo'},
+      {src: sol, alt: 'member logo'},
+      {src: fadear, alt: 'member logo'},
+      {src: fnab, alt: 'member logo'},
+      {src: fne, alt: 'member logo'},
+      {src: mrjc, alt: 'member logo'},
+      {src: fnh, alt: 'member logo'},
+      {src: unaf, alt: 'member logo'},
+      {src: wwf, alt: 'member logo'},
+      {src: fnpnr, alt: 'member logo'},
+      {src: genCob, alt: 'member logo'},
+      {src: gf, alt: 'member logo'},
+      {src: greenpeace, alt: 'member logo'},
+      {src: rac, alt: 'member logo'},
+      {src: isf, alt: 'member logo'},
+      {src: lpo, alt: 'member logo'},
+      {src: miramap, alt: 'member logo'},
+      {src: slowFood, alt: 'member logo'},
+      {src: terreDeLiens, alt: 'member logo'},
+      {src: terreEtHumanisme, alt: 'member logo'}
+    ]
+
+  }
+
+  render () {
+    return (
+      <div id="logo-container" className="logo-container">
+
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{350: 3, 750: 7, 900: 10}}
+        >
+          <Masonry>
+
+            {this.state.logos.map((item, i) =>
+              <div className='member-logo' key={i}>
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                />
+              </div>
+            )}
+
+          </Masonry>
+        </ResponsiveMasonry>
+
+      </div>
+
+    )
+  }
+}
