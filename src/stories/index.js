2ec510849a014e39a6c14d45c8fd50a4af93131c
HediK <hedi0475@gmail.com>
2018-06-03 03:51:07 +0200

    lint fixes

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
+/*
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
-
+*/
 import Footer from '../components/Footer'
 import FooterMembersLogos from '../components/FooterMembersLogos'
 
 storiesOf('Footer', module)
-  .add('Complete footer', () => <Footer /> )
+  .add('Complete footer', () => <Footer />)
 
 storiesOf('FooterMembersLogos', module)
-  .add('Footer members logo section', () => <FooterMembersLogos /> )
+  .add('Footer members logo section', () => <FooterMembersLogos />)
