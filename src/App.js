13faf4a5441c654e7c688a2facadd913fee28ed6
Clement Denis <le.mikmac@gmail.com>
2018-06-05 09:04:52 +0200

    bind store to react App state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import { store } from './store'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = store.getState()
+    store.subscribe(() => {
+      this.setState(store.getState())
+    })
+  }
   render () {
     return (
       <div>
-        <Header />
+        <Header menu={this.state.menu}/>
       </div>
     )
   }
