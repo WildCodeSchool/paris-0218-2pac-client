4492d083a2b393f0ceaa2c9377c8006b35ab5a45
Nadia Djellali <djellali.nadia@gmail.com>
2018-06-06 10:31:29 +0200

    création des composants Submit, Input et Complete Formular sur Story

---
+++ src/components/Submit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Submit.css'
+
+class Submit extends Component {
+  render () {
+    return (
+      <label>
+        <input type='submit' value="➔ S'inscrire" />
+      </label>
+    )
+  }
+}
+
+export default Submit
