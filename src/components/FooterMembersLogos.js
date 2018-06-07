f6dd85b4057978e534097ef0ad9cc59fe7447462
HediK <hedi0475@gmail.com>
2018-06-07 20:06:00 +0200

    fixed footer with new logos and better hamonization

--- src/components/FooterMembersLogos.js
+++ src/components/FooterMembersLogos.js
@@ -1,70 +1,71 @@
 import React from 'react'
 import './Footer.css'
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
-import actionAid from './../img/members_logo/action_aid.png'
-import agter from './../img/members_logo/agter.jpg'
-import ape from './../img/members_logo/ape.jpg'
-import attac from './../img/members_logo/attac.png'
-import bioCons from './../img/members_logo/bio_consomacteurs.png'
-import cen from './../img/members_logo/cen.jpg'
-import cfsi from './../img/members_logo/cfsi.png'
-import civam from './../img/members_logo/civam.png'
-import ciwf from './../img/members_logo/ciwf.jpg'
-import cmr from './../img/members_logo/cmr.jpg'
-import commerceEq from './../img/members_logo/commerce_equitable_france.jpg'
-import conf from './../img/members_logo/conf.png'
-import fadear from './../img/members_logo/fadear.png'
-import fnab from './../img/members_logo/fnab.png'
-import fne from './../img/members_logo/FNE.jpg'
-import fnh from './../img/members_logo/FNH.jpg'
-import fnpnr from './../img/members_logo/fnpnr.png'
-import genCob from './../img/members_logo/generations_cobayes.jpg'
-import gf from './../img/members_logo/gf.png'
-import greenpeace from './../img/members_logo/greenpeace.png'
-import isf from './../img/members_logo/isf.jpg'
-import lpo from './../img/members_logo/lpo.png'
-import miramap from './../img/members_logo/miramap.png'
-import mrjc from './../img/members_logo/mrjc.png'
-import rac from './../img/members_logo/rac.jpg'
-import slowFood from './../img/members_logo/slow_food.jpg'
-import sol from './../img/members_logo/sol.png'
-import terreDeLiens from './../img/members_logo/terre_de_liens.png'
-import terreEtHumanisme from './../img/members_logo/terre_et_humanisme.png'
-import unaf from './../img/members_logo/unaf.jpg'
-import wwf from './../img/members_logo/wwf.jpeg'
+import logos08  from './../img/logos/logos-08.png'
+import logos30  from './../img/logos/logos-30.png'
+import logos07  from './../img/logos/logos-07.png'
+import logos25  from './../img/logos/logos-25.png'
+import logos21  from './../img/logos/logos-21.png'
+import logos13  from './../img/logos/logos-13.png'
+import logos29  from './../img/logos/logos-29.png'
+import logos16  from './../img/logos/logos-16.png'
+import logos27  from './../img/logos/logos-27.png'
+import logos31  from './../img/logos/logos-31.png'
+import logos11  from './../img/logos/logos-11.png'
+import logos05  from './../img/logos/logos-05.png'
+import logos03  from './../img/logos/logos-03.png'
+import logos14  from './../img/logos/logos-14.png'
+import logos09  from './../img/logos/logos-09.png'
+import logos15  from './../img/logos/logos-15.png'
+import logos01  from './../img/logos/logos-01.png'
+import logos17  from './../img/logos/logos-17.png'
+import logos18  from './../img/logos/logos-18.png'
+import logos06  from './../img/logos/logos-06.png'
+import logos23  from './../img/logos/logos-23.png'
+import logos26  from './../img/logos/logos-26.png'
+import logos12  from './../img/logos/logos-12.png'
+import logos04  from './../img/logos/logos-04.png'
+import logos28  from './../img/logos/logos-28.png'
+import logos20  from './../img/logos/logos-20.png'
+import logos19  from './../img/logos/logos-19.png'
+import logos24  from './../img/logos/logos-24.png'
+import logos22  from './../img/logos/logos-22.png'
+import logos02  from './../img/logos/logos-02.png'
+import logos10  from './../img/logos/logos-10.png'
 
 const logos = [
-  {src: actionAid, alt: 'member logo'},
-  {src: agter, alt: 'member logo'},
-  {src: ape, alt: 'member logo'},
-  {src: attac, alt: 'member logo'},
-  {src: bioCons, alt: 'member logo'},
-  {src: cen, alt: 'member logo'},
-  {src: cfsi, alt: 'member logo'},
-  {src: civam, alt: 'member logo'},
-  {src: ciwf, alt: 'member logo'},
-  {src: cmr, alt: 'member logo'},
-  {src: commerceEq, alt: 'member logo'},
-  {src: conf, alt: 'member logo'},
-  {src: sol, alt: 'member logo'},
-  {src: fadear, alt: 'member logo'},
-  {src: fnab, alt: 'member logo'},
-  {src: fne, alt: 'member logo'},
-  {src: mrjc, alt: 'member logo'},
-  {src: fnh, alt: 'member logo'},
-  {src: unaf, alt: 'member logo'},
-  {src: wwf, alt: 'member logo'},
-  {src: fnpnr, alt: 'member logo'},
-  {src: genCob, alt: 'member logo'},
-  {src: gf, alt: 'member logo'},
-  {src: greenpeace, alt: 'member logo'},
-  {src: rac, alt: 'member logo'},
-  {src: isf, alt: 'member logo'},
-  {src: lpo, alt: 'member logo'},
-  {src: miramap, alt: 'member logo'},
-  {src: slowFood, alt: 'member logo'},
-  {src: terreDeLiens, alt: 'member logo'},
-  {src: terreEtHumanisme, alt: 'member logo'}
+  {src: logos08, alt: 'logos08', title: 'logos08', },
+  {src: logos07, alt: 'logos07', title: 'logos07', },
+  {src: logos30, alt: 'logos30', title: 'logos30', },
+  {src: logos21, alt: 'logos21', title: 'logos21', },
+  {src: logos13, alt: 'logos13', title: 'logos13', },
+  {src: logos29, alt: 'logos29', title: 'logos29', },
+  {src: logos19, alt: 'logos19', title: 'logos19', },
+  {src: logos16, alt: 'logos16', title: 'logos16', },
+  {src: logos27, alt: 'logos27', title: 'logos27', },
+  {src: logos31, alt: 'logos31', title: 'logos31', },
+  {src: logos11, alt: 'logos11', title: 'logos11', },
+  {src: logos05, alt: 'logos05', title: 'logos05', },
+  {src: logos03, alt: 'logos03', title: 'logos03', },
+  {src: logos14, alt: 'logos14', title: 'logos14', },
+  {src: logos09, alt: 'logos09', title: 'logos09', },
+  {src: logos22, alt: 'logos22', title: 'logos22', },
+  {src: logos15, alt: 'logos15', title: 'logos15', },
+  {src: logos26, alt: 'logos26', title: 'logos26', },
+  {src: logos12, alt: 'logos12', title: 'logos12', },
+  {src: logos01, alt: 'logos01', title: 'logos01', },
+  {src: logos20, alt: 'logos20', title: 'logos20', },
+  {src: logos04, alt: 'logos04', title: 'logos04', },
+  {src: logos24, alt: 'logos24', title: 'logos24', },
+  {src: logos17, alt: 'logos17', title: 'logos17', },
+  {src: logos02, alt: 'logos02', title: 'logos02', },
+  {src: logos25, alt: 'logos25', title: 'logos25', },
+  {src: logos28, alt: 'logos28', title: 'logos28', },
+  {src: logos23, alt: 'logos23', title: 'logos23', },
+  {src: logos18, alt: 'logos18', title: 'logos18', },
+  {src: logos06, alt: 'logos06', title: 'logos06', },
+  {src: logos10, alt: 'logos10', title: 'logos10', },
+
 ]
 
 const FooterMembersLogos = () => {
@@ -72,13 +73,14 @@ const FooterMembersLogos = () => {
 
     <div id="logo-container" className="logo-container">
       <ResponsiveMasonry
-        columnsCountBreakPoints={{100: 3, 350: 5, 750: 7, 900: 10}}
+        columnsCountBreakPoints={{100: 3, 350: 8, 750: 9, 900: 10}}
       >
         <Masonry>
           {logos.map((item, i) =>
             <div className='member-logo' key={i}>
               <img
                 src={item.src}
+                title={item.title}
                 alt={item.alt}
               />
             </div>
