52fcbc123336198bd6ed30f82c9ca074a635fb21
Helene Blanchard <erba0614@gmail.com>
2018-06-11 15:06:27 +0200

    SideBars Left and Right final

--- src/components/AsideLeft.js
+++ src/components/AsideLeft.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react'
 import './AsideLeft.css'
 
 class AsideLeft extends Component {
-
-  render(){
-         return(
-          <div className="AsideLeft_Line">
-          <span>CATEGORIE</span>
-          <h4>Titre article</h4>
-          <p>Texte de l'article</p>
-           <a href="#lien vers la page de l'article">>>> Lire la suite</a>
-           </div>
-                )
-          }
+  render () {
+    return (
+      <div className="AsideLeft_Line">
+        <h1><span>>>></span>FIL D'ACTUALITÉS</h1>
+        <span className="category"><b>CATÉGORIE</b></span>
+        <span className="title"><b>Titre de l'article</b></span>
+        <p>C'est le texte de l'article C'est le texte de l'article C'est le texte de l'article</p>
+        <p><a className="mb" href="#lien vers la page de l'article">>>> Lire la suite</a></p>
+      </div>
+    )
+  }
 }
 
-
-  export default AsideLeft;
+export default AsideLeft
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,20 +5,20 @@ import SideBarButton from './SideBarButton'
 const SideBar = () => {
   return (
     <div className="SideBar icon-button-globe">
-      <SideBarButton onClick={() => console.log('Actualités')}  style={{ backgroundColor: '#226462' }}>
+      <SideBarButton onClick={() => console.log('Actualités')} style={{ backgroundColor: '#226462' }}>
         <span><i className="fas fa-globe"></i></span>Actualités
       </SideBarButton>
       <SideBarButton onClick={() => console.log('Ressources')} style={{ backgroundColor: '#F4971A' }}>
         <span><i className="fas fa-book-open"></i></span>Ressources
       </SideBarButton>
       <SideBarButton onClick={() => console.log('Contacts')} style={{ backgroundColor: '#A99C2F' }}>
-        <span><i className="fas fa-user-alt"></i></span>Contacts
+        <span><i className="fas fa-user-alt"></i></span>&nbsp;&nbsp;&nbsp;Contacts&nbsp;
       </SideBarButton>
       <SideBarButton onClick={() => console.log('Newsletter')} style={{ backgroundColor: '#731717' }}>
         <span><i className="fas fa-rss"></i></span>Newsletter
       </SideBarButton>
-      <SideBarButton onClick={() => console.log('Twitter')} style={{ backgroundColor: 'grey', width: '2em', height: '2em', position: 'relative'  }}>
-        <span><i className="twitter" className="fab fa-twitter"></i></span>
+      <SideBarButton onClick={() => console.log('Twitter')} style={{ backgroundColor: 'grey', width: '2.2em', height: '2.2em', marginLeft: '4em', paddingLeft: '0.6em' }}>
+        <span><i className="fab fa-twitter"></i></span>
       </SideBarButton>
     </div>
   )
