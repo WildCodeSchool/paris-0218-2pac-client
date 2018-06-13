128794379eac85520c203ff5f83fd9ebc1d782fa
Sichelle Chiffert <sichelle.chiffert@gmail.com>
2018-06-13 12:26:41 +0200

    oublie de run lint dsl

--- src/components/PreviewArticle.js
+++ src/components/PreviewArticle.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react'
-import Container from './Container.js'
 import './PreviewArticle.css'
 
 class PreviewArticle extends Component {
   render () {
-  return (
+    return (
       <div>
-      <h1 className='titleArticle'>Titre de l'article</h1>
-      <div className='uneArticle'> Victus universis caro ferina est lactisque abundans copia qua
+        <h1 className='titleArticle'>Titre de l'article</h1>
+        <div className='uneArticle'> Victus universis caro ferina est lactisque abundans copia qua
           sustentantur, et herbae multiplices et siquae alites capi per
           aucupium possint.
-      </div>
+        </div>
       </div>
     )
   }
