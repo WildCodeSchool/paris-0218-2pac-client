5d8d98b1d2009c134af21711f7abd27624a30af5
Helene Blanchard <erba0614@gmail.com>
2018-06-18 20:25:12 +0200

    Ajout du proxy client, ajout des props au composant Asideleft, fetch des articles

--- src/App.js
+++ src/App.js
@@ -2,35 +2,34 @@ import React, { Component } from 'react'
 import Header from './components/Header'
 import SideBar from './components/SideBar'
 import Footer from './components/Footer'
-//import AsideLeft from './components/AsideLeft'
+// import AsideLeft from './components/AsideLeft'
 import './App.css'
-
 
 class App extends Component {
   state: {
     articles: []
     }
 
-  componentDidMount() {
-  fetch('/auth/getArticles')
-   .then (response => response.json())
-   .then (data => { console.log(data)
-    this.setState({ articles: data })})
-      }
+  componentDidMount () {
+    fetch('/auth/getArticles')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ articles: data })
+      })
+  }
 
   render () {
     console.log('render', this.state)
-     return (
+    return (
       <div>
         <Header />
         <SideBar />
-        {/*<AsideLeft />*/}
+        {/* <AsideLeft /> */}
         <Footer />
       </div>
-    );
+    )
   }
 }
-
-
 
 export default App
--- src/components/AsideLeft.js
+++ src/components/AsideLeft.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import './AsideLeft.css'
 
-const AsideLeft = props =>
- {
-        <div className="AsideLeft_Line">
-        <h1><span>>>></span>FIL D'ACTUALITÉS</h1>
-        <span className="category"><b>{props.categoryId}</b></span>
-        <span className="title"><b>{props.title}</b></span>
-        <p>{props.shortDescription}</p>
-        <p><a className="mb" href="#lien vers la page de l'article">>>> Lire la suite</a></p>
-      </div>
-  }
+const AsideLeft = props => {
+  return (
+    <div className="AsideLeft_Line">
+      <h1><span>>>></span>FIL D'ACTUALITÉS</h1>
+      <span className="category"><b>{props.categoryId}</b></span>
+      <span className="title"><b>{props.title}</b></span>
+      <p>{props.shortDescription}</p>
+      <p><a className="mb" href="#lien vers la page de l'article">>>> Lire la suite</a></p>
+    </div>
+  )
+}
 
 export default AsideLeft
