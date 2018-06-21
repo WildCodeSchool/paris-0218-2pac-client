7aa204beddcb07e9849cc523a2c63e7e96179f2f
frederic <fredydope@gmail.com>
2018-06-21 09:59:55 +0200

    ajout de material ui sur paccalendar

--- src/components/PacCalendar.js
+++ src/components/PacCalendar.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Calendar from 'react-calendar'
 import ModalEvent from './Calendar/ModalEvent'
 import DatePicker from 'react-date-picker'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'
+import MenuItem from '@material-ui/core/MenuItem'
 
 class PacCalendar extends React.Component {
   state = {
@@ -11,15 +14,37 @@ class PacCalendar extends React.Component {
   render () {
     return (
       <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
-        <ModalEvent />
-        <DatePicker
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+      <Grid container
+            alignItems='center'
+            style={{ height:  '100%' }}
+                >
+                    <Grid  item  xs={12}
+                    >
+                        <Paper
+                        elevation={4}
+                        style={{ margin:  32 }}
+                        >
+                            <Grid
+                            container
+                            alignContent='center'
+                            justify='center'
+                            >
+                            <Grid   item  xs={12}  sm={6}
+                                    style={{ 'textAlign':  'center' }}
+                            >
+                            <Calendar
+                                  onChange={this.onChange}
+                                  value={this.state.date}
+                                  />
+                            </Grid>
+                                <Grid
+                                item  xs={12} sm={6}>
+                                  <ModalEvent />
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Grid>
+                </Grid>
       </div>
     )
   }
