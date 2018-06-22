73060139a12deafdc112f6b9fe885106a3df3cc0
HediK <hedi0475@gmail.com>
2018-06-22 14:07:25 +0200

    added the Newsletter component/container for the router

---
+++ src/containers/NewsletterContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpFormular from '../components/SignUpFormular'
+
+const NewsletterContainer = (props) => {
+  return (
+
+    <div className="router-container">
+      <SignUpFormular />
+    </div>
+
+  )
+}
+
+export default NewsletterContainer
