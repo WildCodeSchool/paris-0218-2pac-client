84bfdf986534039cc89a618cc7a1c96738da47d8
HediK <hedi0475@gmail.com>
2018-06-27 20:22:58 +0200

    modified css in the attempt to fix some issues

--- src/components/Actuality.js
+++ src/components/Actuality.js
@@ -9,12 +9,10 @@ const Actuality = ({ ActuData }) => {
     <div className="actu-container">
       {ActuData.map(article =>
         <Link to={`/article/${article.id}`}>
-          <div className="effect-test">
             <div className="actuality" style={{ backgroundImage: `url(${article.imageURL})` }} >
               <span category={article.category} className="category">{article.category}</span>
               <ArticlePreview title={article.title} shortDescription={article.shortDescription} />
             </div>
-          </div>
         </Link>
       )}
 
