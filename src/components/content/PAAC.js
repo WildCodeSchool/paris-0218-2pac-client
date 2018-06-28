89b3498b339493010a4930f7bdf28aa6c884378c
Nadia Djellali <djellali.nadia@gmail.com>
2018-06-28 14:56:45 +0200

    intégration html notre vision

---
+++ src/components/content/PAAC.js
@@ -0,0 +1,264 @@
+import React from 'react'
+import './Content.css'
+
+const PAAC = ({children}) => {
+  return (
+      <div className="container-content">
+        <p class="p1">
+  <span class="s1">
+    Notre vision – Nos 12 priorités pour la PAAC post 2020
+  </span>
+</p>
+<p class="p2">
+   
+</p>
+<p class="p3">
+  <span class="s1">
+    Osons une vraie réforme de la PAC, pour le bénéfice de tous·tes les citoyen·ne·s européen·ne·s !
+  </span>
+</p>
+<p class="p4">
+  <span class="s1">
+    Forte de son budget annuel de plus de 50 milliards €, la Politique Agricole Commune (PAC) de l’Union européenne (UE) structure le système agro-alimentaire européen. Aujourd’hui, une majorité de son budget est dépensée au profit de l’agriculture intensive et de pratiques qui nuisent au développement durable des territoires ruraux, à la santé et à l’environnement, privant ainsi les citoyen·ne·s européen·ne·s d’une utilisation judicieuse de l’argent public. À partir de 2021, une nouvelle PAC doit entrer en vigueur. Les négociations pour en définir les contours ont déjà commencé. Ce processus constitue une véritable opportunité pour la rendre plus juste, plus durable et plus cohérente.
+  </span>
+</p>
+<p class="p4">
+  <span class="s1">
+    La future politique agricole et alimentaire commune (PAAC) que la plateforme
+    <em>
+      Pour une autre PAC
+    </em>
+     défend permettra à tou·te·s les paysan·ne·s volontaires de l’UE d'entrer dans la transition agroécologique des modèles agricoles et alimentaires européens. À horizon 2030, une refonte de la PAC plus ambitieuse achèvera alors de réorienter le système agro-alimentaire de l’UE vers une prise en compte totale des enjeux de revenu agricole, d’alimentation, de santé, de dynamisation des campagnes, d’environnement, de bien-être animal, comme des principes de solidarité et de démocratie.
+  </span>
+</p>
+<p class="p5">
+   
+</p>
+<p class="p6">
+  <span class="s1">
+    <em>
+       « Citoyen·ne·s et alimentation »
+    </em>
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Co-construire la PAC avec les citoyen·ne·s et les acteur·trice·s publics de l’environnement et de la santé
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Ouvrir les instances de gouvernance à la société civile et aux décideur·se·s en charge de l’environnement et de la santé à toutes les échelles territoriales
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Soutenir les productions en faveur d’une alimentation saine et de qualité
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Créer des aides couplées pour les fruits et légumes frais et les légumineuses produits en agroécologie
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Développer des dynamiques locales d’approvisionnement alimentaire qui répondent aux attentes des citoyen·ne·s
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Rendre éligible dans le second pilier les projets de développement de filières territoriales de production ou de transformation et les projets alimentaires territoriaux (PAT)
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Rendre l’agriculture biologique accessible à tou·te·s
+    </strong>
+  </span>
+</p>
+<p class="p9">
+  <span class="s1">
+    Attribuer des financements importants pour le maintien et le développement de l’agriculture
+  </span>
+  <span class="s1">
+    biologique
+  </span>
+</p>
+<p class="p10">
+   
+</p>
+<p class="p6">
+  <span class="s1">
+    <em>
+       « Paysan·ne·s et fermes »
+    </em>
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Stimuler l’emploi agricole plutôt que l’agrandissement des fermes
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Plafonner toutes les aides à l’actif
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Accompagner l’installation de tous les projets paysans
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Relever l’âge limite pour percevoir l’aide à l’installation et adapter l’accompagnement proposé dans le parcours pré·installation à chaque projet
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Donner de vrais moyens aux paysan·ne·s de se protéger face à la volatilité des prix et leur garantir ainsi un revenu
+    </strong>
+  </span>
+</p>
+<p class="p9">
+  <span class="s1">
+    Mettre en œuvre des dispositifs efficaces de régulation des marchés et de maîtrise des volumes de production
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Mettre fin aux importations et exportations qui nuisent aux paysan·ne·s dans les pays du Sud
+    </strong>
+  </span>
+</p>
+<p class="p9">
+  <span class="s1">
+    Refuser tout nouvel accord de libre·échange et mettre en place un dispositif de remboursement des aides PAC pour les matières premières exportées en dehors de l’UE
+  </span>
+</p>
+<p class="p11">
+   
+</p>
+<p class="p6">
+  <span class="s1">
+    <em>
+       « Transition agroécologique »
+    </em>
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Rendre les conditions d’octroi des aides lisibles et efficaces par rapport aux objectifs visés
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Adapter la conditionnalité pour rehausser l’ambition des critères environnementaux et créer une conditionnalité sociale et de bien-être animal
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Reconnaître les pratiques particulièrement vertueuses pour l’environnement en les rémunérant
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Allouer au moins 40% du budget du premier pilier aux paiements pour services environnementaux
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Financer la transition agroécologique des fermes, et notamment la sortie des pesticides
+    </strong>
+  </span>
+</p>
+<p class="p8">
+  <span class="s1">
+    Accorder une part significative du budget à des mesures agro- à environnementales et climatiques ainsi qu’à des aides à la diversification et à l’autonomie des fermes
+  </span>
+</p>
+<p class="p7">
+  <span class="s1">
+    <strong>
+      Gérer les risques sanitaires et climatiques en amont, en encourageant des fermes qui s’y adaptent
+    </strong>
+  </span>
+</p>
+<p class="p9">
+  <span class="s1">
+    Renoncer à tout financement public des assurances privées et financer à la place l’amélioration de la résilience des fermes
+  </span>
+</p>
+<p class="p12">
+   
+</p>
+<p class="p8">
+  <span class="s1">
+    Les 12 priorités pour la PAAC post 2020 de la plateforme
+    <em>
+      Pour une autre PAC
+    </em>
+     à télécharger
+  </span>
+</p>
+<p class="p2">
+   
+</p>
+<p class="p13">
+  <span class="s1">
+    Notre vision – La PAAC post 2020
+  </span>
+</p>
+<p class="p12">
+   
+</p>
+<p class="p8">
+  <span class="s1">
+    PAAC post 2020 souhaitée par la plateforme
+    <em>
+      Pour une autre PAC
+    </em>
+     à télécharger
+  </span>
+</p>
+<p class="p14">
+   
+</p>
+<p class="p15">
+   
+</p>
+<p class="p13">
+  <span class="s1">
+    Notre vision – La PAAC à horizon 2030
+  </span>
+</p>
+<p class="p12">
+   
+</p>
+<p class="p8">
+  <span class="s1">
+    PAAC souhaitée par la plateforme <i>Pour une autre PAC </i> à télécharger
+  </span>
+</p>
+      </div>
+
+export default PAAC
