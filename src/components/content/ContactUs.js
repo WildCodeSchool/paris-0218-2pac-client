e931894048f5eb5f646a52552ca314f9e5729f3d
Edouard de Laage <edouarddelaage@gmail.com>
2018-06-28 14:52:45 +0200

    Add all content fix

---
+++ src/components/content/ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Content.css'
+
+const ContactUs = ({children}) => {
+  return (
+    <div className="container-content">
+      <h2>Content in English - Contact us</h2>
+      <p>For any question, please contact us by email at <a href="mailto:aurelie.catallo@pouruneautrepac.eu"> aurelie.catallo[@]pouruneautrepac.eu </a> or by phone: +33 180 89 99 51.</p>
+      <p>English and German writing or speaking welcome!</p>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ src/components/content/TheCapInFrance.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Content.css'
+
+const TheCapInFrance = ({children}) => {
+  return (
+    <div className="container-content">
+      <h2>Content in English - The CAP in France</h2>
+      <p>For a summary of France’s choices regarding the implementation of the CAP 2014-2020, you can read the following factsheets from the European Commission : <a href="https://ec.europa.eu/agriculture/sites/agriculture/files/cap-in-your-country/pdf/fr_en.pdf"> https://ec.europa.eu/agriculture/sites/agriculture/files/cap-in-your-country/pdf/fr_en.pdf </a> .</p>
+      <p> </p>
+      <p>Download the contribution of the French government to the preparation of the future post 2020 CAP</p>
+    </div>
+  )
+}
+
+export default TheCapInFrance
