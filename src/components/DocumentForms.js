a16725802d48f1df4f35693e7f3b2ba9f643d775
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-04 15:37:33 +0200

    NewDocumentForm

---
+++ src/components/DocumentForms.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import Container from './Container'
+import './DocumentForms.css'
+
+const initialInputValues = {
+  title: '',
+  shortDescription: '',
+  typeId: 1,
+  isMemberOnly: false,
+  isResource: false,
+}
+
+const fakeInputValues = {
+  title: 'Fake document',
+  shortDescription: 'fake fake',
+  typeId: 3,
+  isMemberOnly: true,
+  isResource: false,
+}
+
+export class NewDocumentForm extends React.Component {
+  state = {
+    inputs: initialInputValues,
+    // inputs: fakeInputValues,
+  }
+
+  handleChange = e => {
+    switch (e.target.name) {
+      case 'document':
+        return this.setState({ inputs: { ...this.state.inputs, document: e.target.files[0] } })
+      default:
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+        return this.setState({ inputs: { ...this.state.inputs, [e.target.name]: value } })
+    }
+  }
+
+  reset = () => {
+    this.setState({ inputs: initialInputValues })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const inputs = this.state.inputs
+
+    const formData = new FormData()
+
+    for (const [key, value] of Object.entries(inputs)) {
+      formData.set(key, value)
+    }
+
+    fetch('http://localhost:5000/documents', {
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(res => this.reset())
+  }
+
+  render () {
+    const inputs = this.state.inputs
+
+    return (
+      <div>
+        <Container>
+          <form id='documents_form' onSubmit={this.handleSubmit}>
+            <label>Titre
+              <input type="text_title" name='title' value={inputs.title} onChange={this.handleChange} />
+            </label>
+            <label>Document
+              <input id="doc" type="file" name="document" onChange={this.handleChange} accept="image/png, image/jpeg, application/pdf, .word, .ppt, .pdf, .docx" />
+            </label>
+            <label>Catégorie
+              <select id="typeId" name='typeId' value={inputs.typeId} onChange={this.handleChange}>
+                <option value={1}>Textes politiques</option>
+                <option value={2}>Documents de position des organisations membres</option>
+                <option value={3}>Documents réservés aux membres</option>
+                <option value={4}>Travaux de recherche</option>
+                <option value={5}>Autres documents de position</option>
+              </select>
+            </label>
+            <label>Description
+              <textarea type="text_resume" name='shortDescription' value={inputs.shortDescription} onChange={this.handleChange} />
+            </label>
+            <label>Accessible uniquement aux membres
+              <input type='checkbox' name='isMemberOnly' checked={inputs.isMemberOnly} onChange={this.handleChange} />
+            </label>
+            <label>Ressource
+              <input type='checkbox' name='isResource' checked={inputs.isResource} onChange={this.handleChange} />
+            </label>
+            <label>
+              <button type="submit" value="submit">Ajouter</button>
+            </label>
+          </form>
+        </Container>
+      </div>
+    )
+  }
+}
