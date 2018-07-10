4cb870be95e88a259cd497011cf8bb2c6548213b
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-10 18:17:48 +0200

    api - _fetch & _fetch.authenticated

--- src/api.js
+++ src/api.js
@@ -2,27 +2,26 @@ import jwt from './jwt'
 
 const hostUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000'
 
-// Datas
+const _fetch = (route, options = {}) => fetch(route, options)
+  .then(async res => {
 
-const getArticles = () => fetch('/articles')
-  .then(response => response.json())
+    if (res.status >= 400) {
+      const json = await res.json()
 
-const getDocuments = () => fetch('/documents')
-  .then(response => response.json())
+      throw Error(json.error || res.statusText)
+    }
 
-const getSubscribers = () => fetch('/subscribers')
-  .then(response => response.json())
+    return res.json()
+  })
 
-// AUTH
-
-const authenticatedFetch = (route, options = {}) => {
+_fetch.authenticated = (route, options = {}) => {
   const token = jwt.get()
 
   if (!token) {
     return Promise.reject(Error(`missing token for authenticated fetch '${route}'`))
   }
 
-  return fetch(route, {
+  return _fetch(route, {
     ...options,
     headers: {
       ...options.headers,
@@ -36,18 +35,16 @@ const whoami = () => {
 
   if (!token) { return Promise.resolve({}) }
 
-  return authenticatedFetch('/whoami')
-    .then(response => response.json())
+  return _fetch.authenticated('/whoami')
 }
 
-const signIn = credentials => fetch('/signin', {
+const signIn = credentials => _fetch('/signin', {
   method: 'post',
   headers: {
     'content-type': 'application/json'
   },
   body: JSON.stringify(credentials)
 })
-  .then(response => response.json())
   .then(response => {
     if (response.error) { return response }
 
@@ -67,8 +64,20 @@ const signOut = () => {
   return Promise.resolve()
 }
 
+
+// Datas
+
+const getUsers = () => _fetch.authenticated('/users')
+
+const getArticles = () => _fetch('/articles')
+
+const getDocuments = () => _fetch('/documents')
+
+const getSubscribers = () => _fetch('/subscribers')
+
 export default {
   hostUrl,
+  getUsers,
   getArticles,
   getDocuments,
   getSubscribers,
