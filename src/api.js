fb1312e4bd523031bd9dac8b9daf803f0ee52fcc
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-10 11:48:39 +0200

    Admin area authentication system (form)

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 const hostUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000'
 
+// Datas
+
 const getArticles = () => fetch('/articles')
   .then(response => response.json())
 
@@ -9,9 +11,70 @@ const getDocuments = () => fetch('/documents')
 const getSubscribers = () => fetch('/subscribers')
   .then(response => response.json())
 
+
+// AUTH
+
+// helpers
+
+const LOCALSTORAGE_JWT = 'JWT'
+
+const getToken = () => localStorage.getItem(LOCALSTORAGE_JWT)
+
+const authenticatedFetch = (route, options = {}) => {
+  const token = getToken()
+
+  if (!token) {
+    return Promise.reject(Error(`missing token for authenticated fetch '${route}'`))
+  }
+
+  return fetch(route, {
+    ...options,
+    headers: {
+      ...options.headers,
+      'Authorization': `JWT ${token}`
+    }
+  })
+}
+
+const whoami = () => {
+  if (!getToken()) { return Promise.resolve({}) }
+
+  return authenticatedFetch('/whoami')
+    .then(response => response.json())
+}
+
+const signIn = credentials => fetch('/signin', {
+  method: 'post',
+  headers: {
+    'content-type': 'application/json'
+  },
+  body: JSON.stringify(credentials)
+})
+.then(response => response.json())
+.then(response => {
+  if (response.error) { return response }
+
+  const { user, token } = response
+
+  if (!token) { throw Error('Missing JWT in response!') }
+  if (!user) { throw Error('Missing User in response!') }
+
+  localStorage.setItem(LOCALSTORAGE_JWT, token)
+
+  return response
+})
+
+const signOut = () => {
+  localStorage.removeItem(LOCALSTORAGE_JWT)
+  return Promise.resolve()
+}
+
 export default {
   hostUrl,
   getArticles,
   getDocuments,
-  getSubscribers
+  getSubscribers,
+  signIn,
+  whoami,
+  signOut,
 }
--- src/containers/AdminContainer.js
+++ src/containers/AdminContainer.js
@@ -4,12 +4,85 @@ import { Router } from '@reach/router'
 import api from '../api.js'
 // import '../App.css'
 
-const AdminHome = () => <div>Admin</div>
+const AdminHome = () => <div>Admin zone</div>
 const AdminArticles = () => <div>Articles</div>
 const AdminDocuments = () => <div>Documents</div>
 const AdminSubscribers = () => <div>Subscribers</div>
 
+class AdminAuth extends Component {
+  state = {
+    username: '',
+    password: '',
+    message: '',
+    loggedAs: undefined,
+  }
+
+  handleChange = e => {
+    const { name: key, value } = e.target
+
+    this.setState({ [key]: value })
+  }
+
+  signin = e => {
+    e.preventDefault()
+
+    this.setState({ message: '' })
+
+    const credentials = {
+      username: this.state.username,
+      password: this.state.password,
+    }
+
+    api.signIn(credentials)
+      .then(response => {
+        if (response.error) {
+          this.setState({ username: '', password: '', message: response.error })
+          return
+        }
+
+        this.setState({ loggedAs: response.user })
+      })
+  }
+
+  signout = e => {
+    e.preventDefault()
+
+    api.signOut()
+      .then(() => this.setState({ loggedAs: undefined }))
+  }
+
+  componentDidMount () {
+    api.whoami()
+      .then(response => this.setState({ message: response.error || '', loggedAs: response.user }))
+      .catch(console.log)
+  }
+
+  render () {
+    return (
+      <div>
+      { this.state.loggedAs
+        ?
+        <div>
+          <span>Logged as <strong>{this.state.loggedAs.username}</strong></span>
+          <input type='button' value='Sign Out' onClick={this.signout}/>
+        </div>
+        :
+        <div>
+          <form onSubmit={this.signin}>
+            <input type='text' placeholder='username' name='username' value={this.state.username} onChange={this.handleChange} />
+            <input type='password' placeholder='password' name='password' value={this.state.password} onChange={this.handleChange} />
+            <input type='submit' value='Sign in' />
+          </form>
+          <div>{this.state.message}</div>
+        </div>
+      }
+      </div>
+    )
+  }
+}
+
 class AdminContainer extends Component {
+
   state = {
     articles: [],
     documents: []
@@ -27,12 +100,15 @@ class AdminContainer extends Component {
 
   render () {
     return (
-      <Router>
-        <AdminHome path="/" />
-        <AdminArticles path='articles' />
-        <AdminDocuments path='documents' />
-        <AdminSubscribers path='subscribers' />
-      </Router>
+      <div>
+        <AdminAuth />
+        <Router>
+          <AdminHome path="/" />
+          <AdminArticles path='articles' />
+          <AdminDocuments path='documents' />
+          <AdminSubscribers path='subscribers' />
+        </Router>
+      </div>
     )
   }
 }
