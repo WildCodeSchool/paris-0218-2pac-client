a86ead7414e575c9cfe1470ea4ae8d9bd0c705fb
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-11 22:29:41 +0200

    redux store + reducer

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  loggedAs: undefined
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case 'LOG_IN': return { ...state, loggedAs: action.as }
+
+    case 'LOG_OUT': return { ...state, loggedAs: undefined }
+
+    default: return state
+  }
+}
+
+export default reducer
