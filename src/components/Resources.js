841650a4d898152ec262aa9f6388cdeaf495d0f4
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-11 10:51:35 +0200

    Resources sortByDate

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import api from '../api'
 import './Resources.css'
 
-const Resources = ({ documents }) => {
-  return (
-    <div>
-      {documents.map(doc =>
-        <div key={doc.id} className="resource" >
-          <span>{doc.title}</span>
-          <div className="category_resources">{doc.type}</div>
-          <a target="_blank" href={`${api.hostUrl}/medias/documents/${doc.url}`}>Télécharger</a>
+const ResourceCard = ({ doc }) =>
+  <div className="resource" >
+    <span>{doc.createdAt}</span>
+    <span>{doc.title}</span>
+    <div className="category_resources">{doc.type}</div>
+    <a target="_blank" href={`${api.hostUrl}/medias/documents/${doc.url}`}>Télécharger</a>
+  </div>
+
+class Resources extends Component {
+
+  state = {
+    sortByDate: 1
+  }
+
+  toggleSortByDate = () => {
+    this.setState({ sortByDate: -this.state.sortByDate })
+  }
+
+  render () {
+    const documents = this.props.documents
+    const resourceCards = documents
+      .sort((a, b) => this.state.sortByDate * ((new Date(b.createdAt)) - (new Date(a.createdAt))))
+      .map(doc => <ResourceCard key={doc.id} doc={doc} />)
+
+    return (
+      <div>
+        <div className="resource-filters">
+          <input type="button" value={`${this.state.sortByDate > 0 ? '▼' : '▲'} date`} onClick={this.toggleSortByDate} />
         </div>
-      )}
-    </div>
-  )
+        <div className="resource-cards">{resourceCards}</div>
+      </div>
+    )
+  }
 }
 
 export default Resources
