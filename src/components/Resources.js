708d3b949da6bf870c565f79f20fe4264cbaf0f3
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-11 11:06:34 +0200

    Resources - filters.typeId added

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -13,25 +13,51 @@ const ResourceCard = ({ doc }) =>
 class Resources extends Component {
 
   state = {
-    sortByDate: 1
+    sortByDate: 1,
+    filters: {
+      typeId: 0
+    },
   }
 
   toggleSortByDate = () => {
     this.setState({ sortByDate: -this.state.sortByDate })
   }
 
+  handleFilterChange = e => {
+    const { name, value } = e.target
+
+    this.setState({ filters: { ...this.state.filters, [name]: value }})
+  }
+
   render () {
     const documents = this.props.documents
+
+    const { filters, sortByDate } = this.state
+
     const resourceCards = documents
-      .sort((a, b) => this.state.sortByDate * ((new Date(b.createdAt)) - (new Date(a.createdAt))))
+      .filter(doc => !filters.typeId || filters.typeId == doc.typeId)
+      .sort((a, b) => sortByDate * ((new Date(b.createdAt)) - (new Date(a.createdAt))))
       .map(doc => <ResourceCard key={doc.id} doc={doc} />)
 
     return (
       <div>
         <div className="resource-filters">
           <input type="button" value={`${this.state.sortByDate > 0 ? '▼' : '▲'} date`} onClick={this.toggleSortByDate} />
+          <label>type
+            <select name="typeId" value={this.state.filters.typeId} onChange={this.handleFilterChange}>
+              <option value={0}></option>
+              <option value={1}>Textes politiques</option>
+              <option value={2}>Documents de position des organisations membres</option>
+              <option value={3}>Documents réservés aux membres</option>
+              <option value={4}>Travaux de recherche</option>
+              <option value={5}>Autres documents de position</option>
+            </select>
+          </label>
         </div>
-        <div className="resource-cards">{resourceCards}</div>
+        <div className="resource-cards">
+          <span>{resourceCards.length} resultats</span>
+          {resourceCards}
+        </div>
       </div>
     )
   }
