85bc1923fd5e41671650ca6ebb3f87a29697263b
Yoann Cribier <yoann_cribier@hotmail.com>
2018-07-23 18:11:35 +0200

    NewsPage - filter only Article & Actuality categories

--- src/containers/NewsPage.js
+++ src/containers/NewsPage.js
@@ -3,10 +3,12 @@ import ScrollUp from '../containers/utils/ScrollUp'
 import ArticleCard from '../components/ArticleCard'
 import './NewsPage.css'
 
+const isFromCategoryArticleOrActuality = article => article.categoryId === 1 || article.categoryId === 3
 const byMostRecent = (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
 
 const NewsPage = ({ articles }) => {
   const articleCards = articles
+    .filter(isFromCategoryArticleOrActuality)
     .sort(byMostRecent)
     .map(article => <ArticleCard key={article.id} article={article} displayShortDescription />)
 
